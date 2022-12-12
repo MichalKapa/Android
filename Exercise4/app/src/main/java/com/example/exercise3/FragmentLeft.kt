fcc30aa0af885df419bdb11be872f60210b2f254
MichalKapa <michal.kapala05@gmail.com>
2022-12-12 21:51:46 +0100

    Added 3/5 of Exercise4.

--- Exercise4/app/src/main/java/com/example/exercise3/FragmentLeft.kt
+++ Exercise4/app/src/main/java/com/example/exercise3/FragmentLeft.kt
@@ -1,12 +1,13 @@
 package com.example.exercise3
 
-import android.content.Context
+import android.graphics.Typeface
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
@@ -23,6 +24,8 @@ class FragmentLeft : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var checkbox1 = false
+    private var checkbox2 = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +52,116 @@ class FragmentLeft : Fragment() {
         viewButton1.setOnClickListener { view ->
             view.findNavController().navigate(R.id.action_fragmentLeft_to_fragmentCenter)
         }
+
+        registerForContextMenu(requireActivity().findViewById(R.id.buttonLeft2))
+        registerForContextMenu(requireActivity().findViewById(R.id.textView))
+        registerForContextMenu(requireActivity().findViewById(R.id.buttonLeft3))
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        if(v.id == R.id.buttonLeft2)
+        {
+            requireActivity().menuInflater.inflate(R.menu.context_menu_button1, menu)
+        }
+        else if(v.id == R.id.textView)
+        {
+            requireActivity().menuInflater.inflate(R.menu.context_menu_textview, menu)
+        }
+        else if(v.id == R.id.buttonLeft3)
+        {
+            requireActivity().menuInflater.inflate(R.menu.context_menu_button2, menu)
+            menu.findItem(R.id.fontStylePos1).isChecked = checkbox1
+            menu.findItem(R.id.fontStylePos2).isChecked = checkbox2
+        }
+        super.onCreateContextMenu(menu, v, menuInfo)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val contextMenu1 = requireActivity().findViewById<TextView>(R.id.buttonLeft2)
+        val contextMenu2 = requireActivity().findViewById<TextView>(R.id.textView)
+        val contextMenu3 = requireActivity().findViewById<TextView>(R.id.buttonLeft3)
+        val fontI = Typeface.defaultFromStyle(Typeface.ITALIC)
+        val fontB = Typeface.defaultFromStyle(Typeface.BOLD)
+        val fontIB = Typeface.defaultFromStyle(Typeface.BOLD_ITALIC)
+        val fontN = Typeface.defaultFromStyle(Typeface.NORMAL)
+
+        when (item.itemId){
+            R.id.colorPos1 -> {
+                contextMenu1.setBackgroundColor(requireActivity().resources.getColor(R.color.red))
+//                requireActivity().theme.applyStyle(R.style.contextColorRed, true)
+            }
+            R.id.colorPos2 -> {
+                contextMenu1.setBackgroundColor(requireActivity().resources.getColor(R.color.green))
+//                requireActivity().theme.applyStyle(R.style.contextColorGreen, true)
+            }
+            R.id.fontSizePos1 -> {
+                contextMenu2.textSize = 30.0F
+//                requireActivity().theme.applyStyle(R.style.contextFontSize30, true)
+            }
+            R.id.fontSizePos2 -> {
+                contextMenu2.textSize = 20.0F
+//                requireActivity().theme.applyStyle(R.style.contextFontSize20, true)
+            }
+            R.id.fontStylePos1 -> {
+                if (item.isChecked) {
+                    item.isChecked = false
+                    checkbox1 = false
+                    if (contextMenu3.typeface.isItalic) {
+                        contextMenu3.typeface = fontI
+                    }
+                    else {
+                        contextMenu3.typeface = fontN
+                    }
+                }
+                else {
+                    item.isChecked = true
+                    checkbox1 = true
+                    if (contextMenu3.typeface.isItalic) {
+                        contextMenu3.typeface = fontIB
+                    }
+                    else {
+                        contextMenu3.typeface = fontB
+                    }
+//                    requireActivity().theme.applyStyle(R.style.contextFontStyle1, true)
+                }
+            }
+            R.id.fontStylePos2 -> {
+                if (item.isChecked) {
+                    item.isChecked = false
+                    checkbox2 = false
+                    if (contextMenu3.typeface.isBold) {
+                        contextMenu3.typeface = fontB
+                    }
+                    else {
+                        contextMenu3.typeface = fontN
+                    }
+
+                }
+                else {
+                    item.isChecked = true
+                    checkbox2 = true
+                    if (contextMenu3.typeface.isBold) {
+                        contextMenu3.typeface = fontIB
+                    }
+                    else {
+                        contextMenu3.typeface = fontI
+                    }
+//                    requireActivity().theme.applyStyle(R.style.contextFontStyle2, true)
+                }
+            }
+
+
+            else -> {
+                super.onContextItemSelected(item)
+            }
+        }
+        return true
+
     }
 
     companion object {
--- Exercise4/app/src/main/java/com/example/exercise3/MainActivity.kt
+++ Exercise4/app/src/main/java/com/example/exercise3/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.exercise3
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.RadioGroup
@@ -16,14 +20,20 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), Fragment1.OnMyEventListener, Fragment2.OnMyEventListener{
-//    var frag1: Fragment1? = null //StaticFragment.OnSelectListener
-//    var frag2: Fragment2? = null
-//    var myTrans: FragmentTransaction? = null
-//    private val TAG_F1 = "Fragment1"
-//    private val TAG_F2 = "Fragment2"
+
+    private var chosenTheme = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val data : SharedPreferences = getSharedPreferences("my_shared_pref", Context.MODE_PRIVATE)
+        chosenTheme = data.getInt("chosenTheme", 0)
+        when(chosenTheme) {
+            0 -> {this.setTheme(R.style.Theme_Exercise3)}
+            1 -> {this.setTheme(R.style.Theme_Exercise3_2)}
+            2 -> {this.setTheme(R.style.Theme_Exercise3_3)}
+        }
+
         setContentView(R.layout.activity_main)
 
         val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
@@ -35,51 +45,44 @@ class MainActivity : AppCompatActivity(), Fragment1.OnMyEventListener, Fragment2
             AppBarConfiguration(setOf(R.id.fragmentLeft,R.id.fragmentCenter,R.id.fragmentRight, R.id.fragmentX))
         setupActionBarWithNavController(navController,appBarConfig)
         bnNavView.setupWithNavController(navController)
-
-//        bnNavView.setOnItemSelectedListener { item ->
-//            when (item.itemId) {
-//                R.id.fragmentLeft -> navController.navigate(R.id.action_global_to_fragLeft)
-//                R.id.fragmentCenter -> navController.navigate(R.id.action_global_to_fragCenter)
-//                R.id.fragmentRight -> navController.navigate(R.id.action_global_to_fragRight)
-//            }
-//            true
-//        }
-
-//        if (savedInstanceState == null) {
-//            frag1 = Fragment1.newInstance("a", "b")
-//            frag2 = Fragment2.newInstance("a", "b")
-//            myTrans = supportFragmentManager.beginTransaction()
-//            myTrans!!.add(R.id. fragmentContainer, frag1!!, this.TAG_F1)
-//            myTrans!!.detach(frag1!!)
-//            myTrans!!.add(R.id. fragmentContainer, frag2!!, this.TAG_F2)
-//            myTrans!!.detach(frag2!!)
-//            myTrans!!.commit()
-//        } else {
-//            frag1 = supportFragmentManager.findFragmentByTag(this.TAG_F1) as Fragment1?
-//            frag2 = supportFragmentManager.findFragmentByTag(this.TAG_F2) as Fragment2?
-//        }
-
     }
 
     override fun onMyEvent() {
 
     }
 
-//    override fun onSelect(option: Int) {
-//        val myTrans = supportFragmentManager.beginTransaction()
-//        val radioGroup : RadioGroup = findViewById(R.id.radioGroup)
-//        val checkedId = radioGroup.checkedRadioButtonId
-//        when (checkedId) {
-//            R.id.radioButton1 -> {
-//                myTrans.detach(frag2!!)
-//                myTrans.attach(frag1!!)
-//
-//            }
-//            R.id.radioButton2 -> {
-//                myTrans.detach(frag1!!)
-//                myTrans.attach(frag2!!)
-//            }
-//        }
-//        myTrans.commit();
-//    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.app_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.pozycja1 -> {
+                setTheme(R.style.Theme_Exercise3)
+                theme.applyStyle(R.style.Theme_Exercise3, true)
+                chosenTheme = 0
+            }
+            R.id.pozycja2 -> {
+                setTheme(R.style.Theme_Exercise3_2)
+                theme.applyStyle(R.style.Theme_Exercise3_2, true)
+                chosenTheme = 1
+            }
+            R.id.pozycja3 -> {
+                setTheme(R.style.Theme_Exercise3_3)
+                theme.applyStyle(R.style.Theme_Exercise3_3, true)
+                chosenTheme = 2
+            }
+            else -> {
+                super.onContextItemSelected(item)
+            }
+        }
+        val data: SharedPreferences = getSharedPreferences("my_shared_pref", Context.MODE_PRIVATE)
+        val editor = data.edit()
+        editor.putInt("chosenTheme", chosenTheme)
+        editor.apply()
+        super.onContextItemSelected(item)
+        recreate()
+        return false
+    }
 }
