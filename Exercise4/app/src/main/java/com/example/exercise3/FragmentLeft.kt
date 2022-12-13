9af7f5503ffc7e90d123b64efc68ba5c977fa8d4
MichalKapa <michal.kapala05@gmail.com>
2022-12-13 10:13:36 +0100

    Finished 3/5 of Exercise 4.

--- Exercise4/app/src/main/java/com/example/exercise3/FragmentLeft.kt
+++ Exercise4/app/src/main/java/com/example/exercise3/FragmentLeft.kt
@@ -1,5 +1,8 @@
 package com.example.exercise3
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
@@ -26,6 +29,13 @@ class FragmentLeft : Fragment() {
     private var param2: String? = null
     private var checkbox1 = false
     private var checkbox2 = false
+    private var fontStyle: String? = null
+    private var fontSize: String? = null
+    private var color: String? = null
+    private val fontI = Typeface.defaultFromStyle(Typeface.ITALIC)
+    private val fontB = Typeface.defaultFromStyle(Typeface.BOLD)
+    private val fontIB = Typeface.defaultFromStyle(Typeface.BOLD_ITALIC)
+    private val fontN = Typeface.defaultFromStyle(Typeface.NORMAL)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,6 +63,44 @@ class FragmentLeft : Fragment() {
             view.findNavController().navigate(R.id.action_fragmentLeft_to_fragmentCenter)
         }
 
+        val button1: Button = view.findViewById(R.id.buttonLeft2)
+        val textView: TextView = view.findViewById(R.id.textView)
+        val button2: Button = view.findViewById(R.id.buttonLeft3)
+
+        val data : SharedPreferences = requireActivity().getSharedPreferences("my_shared_pref", Context.MODE_PRIVATE)
+        color = data.getString("color", "none")
+        fontSize = data.getString("font_size", "none")
+        fontStyle = data.getString("font_style", "none")
+
+        if (color == "red") {
+            button1.setBackgroundColor(Color.RED)
+        }
+        else if (color == "green") {
+            button1.setBackgroundColor(Color.GREEN)
+        }
+
+        if (fontSize == "30") {
+            textView.textSize = 30F
+        }
+        else if (fontSize == "20") {
+            textView.textSize = 20F
+        }
+
+        if (fontStyle == "IB") {
+            button2.typeface = fontIB
+        }
+        else if (fontStyle == "B") {
+            button2.typeface = fontB
+        }
+        else if (fontStyle == "I") {
+            button2.typeface = fontI
+        }
+        else if (fontStyle == "N") {
+            button2.typeface = fontN
+        }
+
+
+
         registerForContextMenu(requireActivity().findViewById(R.id.buttonLeft2))
         registerForContextMenu(requireActivity().findViewById(R.id.textView))
         registerForContextMenu(requireActivity().findViewById(R.id.buttonLeft3))
@@ -74,8 +122,15 @@ class FragmentLeft : Fragment() {
         else if(v.id == R.id.buttonLeft3)
         {
             requireActivity().menuInflater.inflate(R.menu.context_menu_button2, menu)
+
+            val data : SharedPreferences = requireActivity().getSharedPreferences("my_shared_pref", Context.MODE_PRIVATE)
+            checkbox1 = data.getBoolean("is_checked1", false)
+            checkbox2 = data.getBoolean("is_checked2", false)
+//            fontStyle = data.getString("font_style", "fontN")
+
             menu.findItem(R.id.fontStylePos1).isChecked = checkbox1
             menu.findItem(R.id.fontStylePos2).isChecked = checkbox2
+
         }
         super.onCreateContextMenu(menu, v, menuInfo)
     }
@@ -85,26 +140,27 @@ class FragmentLeft : Fragment() {
         val contextMenu1 = requireActivity().findViewById<TextView>(R.id.buttonLeft2)
         val contextMenu2 = requireActivity().findViewById<TextView>(R.id.textView)
         val contextMenu3 = requireActivity().findViewById<TextView>(R.id.buttonLeft3)
-        val fontI = Typeface.defaultFromStyle(Typeface.ITALIC)
-        val fontB = Typeface.defaultFromStyle(Typeface.BOLD)
-        val fontIB = Typeface.defaultFromStyle(Typeface.BOLD_ITALIC)
-        val fontN = Typeface.defaultFromStyle(Typeface.NORMAL)
+
 
         when (item.itemId){
             R.id.colorPos1 -> {
                 contextMenu1.setBackgroundColor(requireActivity().resources.getColor(R.color.red))
+                color = "red"
 //                requireActivity().theme.applyStyle(R.style.contextColorRed, true)
             }
             R.id.colorPos2 -> {
                 contextMenu1.setBackgroundColor(requireActivity().resources.getColor(R.color.green))
+                color = "green"
 //                requireActivity().theme.applyStyle(R.style.contextColorGreen, true)
             }
             R.id.fontSizePos1 -> {
                 contextMenu2.textSize = 30.0F
+                fontSize = "30"
 //                requireActivity().theme.applyStyle(R.style.contextFontSize30, true)
             }
             R.id.fontSizePos2 -> {
                 contextMenu2.textSize = 20.0F
+                fontSize = "20"
 //                requireActivity().theme.applyStyle(R.style.contextFontSize20, true)
             }
             R.id.fontStylePos1 -> {
@@ -113,9 +169,11 @@ class FragmentLeft : Fragment() {
                     checkbox1 = false
                     if (contextMenu3.typeface.isItalic) {
                         contextMenu3.typeface = fontI
+                        fontStyle = "I"
                     }
                     else {
                         contextMenu3.typeface = fontN
+                        fontStyle = "N"
                     }
                 }
                 else {
@@ -123,9 +181,11 @@ class FragmentLeft : Fragment() {
                     checkbox1 = true
                     if (contextMenu3.typeface.isItalic) {
                         contextMenu3.typeface = fontIB
+                        fontStyle = "IB"
                     }
                     else {
                         contextMenu3.typeface = fontB
+                        fontStyle = "B"
                     }
 //                    requireActivity().theme.applyStyle(R.style.contextFontStyle1, true)
                 }
@@ -136,9 +196,11 @@ class FragmentLeft : Fragment() {
                     checkbox2 = false
                     if (contextMenu3.typeface.isBold) {
                         contextMenu3.typeface = fontB
+                        fontStyle = "B"
                     }
                     else {
                         contextMenu3.typeface = fontN
+                        fontStyle = "N"
                     }
 
                 }
@@ -147,9 +209,11 @@ class FragmentLeft : Fragment() {
                     checkbox2 = true
                     if (contextMenu3.typeface.isBold) {
                         contextMenu3.typeface = fontIB
+                        fontStyle = "IB"
                     }
                     else {
                         contextMenu3.typeface = fontI
+                        fontStyle = "I"
                     }
 //                    requireActivity().theme.applyStyle(R.style.contextFontStyle2, true)
                 }
@@ -160,6 +224,16 @@ class FragmentLeft : Fragment() {
                 super.onContextItemSelected(item)
             }
         }
+        val data: SharedPreferences = requireActivity().getSharedPreferences("my_shared_pref", Context.MODE_PRIVATE)
+        val editor :SharedPreferences.Editor = data.edit()
+        editor.putBoolean("is_checked1", checkbox1)
+        editor.putBoolean("is_checked2", checkbox2)
+        editor.putString("color", color)
+        editor.putString("font_size", fontSize)
+        editor.putString("font_style", fontStyle)
+
+        editor.apply()
+
         return true
 
     }
